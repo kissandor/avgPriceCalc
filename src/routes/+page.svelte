5b2df29e8715f485138838ae9cd3d922ba32a1c3
Sandor Kis <kisandor@hotmail.com>
2023-02-14 23:01:30 +0000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import Egg from "./egg.svelte";
+    let eggs = [
+        {name: 'Olive', source: 'src/img/olive.png'},
+        {name: 'Leghorn', source: 'src/img/leghorn.png'},
+        {name: 'Legbar', source: 'src/img/legbar.png'},
+        {name: 'Burfordd', source: 'src/img/burford.png'},
+        ]
+</script>
+
+
+<div class="container">
+    <h1>Average egg price calculator</h1>
+    {#each eggs as egg}
+        <Egg source={egg.source} eggName={egg.name}/>
+    {/each}
+</div>
+
+<style>
+    h1{
+        text-align: center ;
+        padding-top: 5px;
+    }
+    .container{
+        width: 100%;
+        height: 100vh;
+        padding: 0px 50px;
+        background-color: #F5F4F3;
+        box-sizing: border-box;
+    }
+</style>
---
+++ src/routes/egg.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let source = "src/img/olive.png";
+    export let eggName = "Burford Brown";
+</script>
+<div class="egg">
+    <img src={source} alt="egg">
+    <div class="container">
+      <span><b>{eggName}</b></span>  
+    </div>
+</div>
+
+<style>
+    img{
+        width: 85%;
+        border-radius: 5px;
+    }
+    .egg {
+      box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+      transition: 0.3s;
+      width: 8%;
+      float: left;
+      margin: 10px;
+      padding-top: 10px;
+      border-radius: 10px;  
+      text-align: center;
+      background-color: white;
+    }
+    
+    .egg:hover {
+      box-shadow: 0 8px 16px 0 rgba(0,0,0,0.5);
+    }
+    
+    .container {
+      padding: 2px 4px;
+      text-align: center;
+    }
+    </style>
